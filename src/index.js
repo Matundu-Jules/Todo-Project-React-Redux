fd007a61a0b19c828779e4fb94de1854daa7079c
Matundu Jules-Langa <matundu.pro@gmail.com>
2022-09-16 15:43:25 +0200

    Redux : Create store.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/styles/index.scss'
+import { legacy_createStore as createStore } from 'redux'
+import { todosReducer } from './store/reducers'
+
+// Initialize the Redux Store //
+const store = createStore(todosReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
