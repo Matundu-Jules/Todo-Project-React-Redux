5419c4866ba4ff045833aa9d52beaadb9893151e
Matundu Jules-Langa <matundu.pro@gmail.com>
2022-09-22 19:06:11 +0200

    Modify reducers and default state for async data

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,36 +1,60 @@
 import * as actions from './actions'
 
-export const todos = (state = [], action) => {
+// Modify reducers for async data //
+// Example of the new state :
+// {
+//     todos: {
+//         data: [],
+//         loading: false,
+//         error: null
+//     }
+//     filter: 'SHOW_ALL'
+// }
+
+export const todos = (
+    state = {
+        data: [],
+        loading: false,
+        error: null,
+    },
+    action
+) => {
     switch (action.type) {
         case actions.ADD_TODO: {
-            return [...state, action.todo]
+            return {
+                ...state,
+                data: [...state.data, action.todo],
+            }
         }
         case actions.MODIFY_TODO: {
-            return state.map((t) => {
-                if (t.id === action.index) {
-                    t.name = action.todo
-                }
-                return t
-            })
+            return {
+                ...state,
+                data: state.map((t) =>
+                    t.id === action.index ? { ...t, name: action.todo } : t
+                ),
+            }
         }
         case actions.DELETE_TODO: {
-            return state.filter((t) => t.id !== action.index)
+            return {
+                ...state,
+                data: state.filter((t) => t.id !== action.index),
+            }
         }
         case actions.TOGGLE_ACTIVE_TODO: {
-            return state.map((t) => {
-                if (t.id === action.index) {
-                    t.active = !t.active
-                }
-                return t
-            })
+            return {
+                ...state,
+                data: state.map((t) =>
+                    t.id === action.index ? { ...t, active: !t.active } : t
+                ),
+            }
         }
         case actions.TOGGLE_DONE_TODO: {
-            return state.map((t) => {
-                if (t.id === action.index) {
-                    t.done = !t.done
-                }
-                return t
-            })
+            return {
+                ...state,
+                data: state.map((t) =>
+                    t.id === action.index ? { ...t, done: !t.done } : t
+                ),
+            }
         }
         default: {
             return state
